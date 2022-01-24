41b7c3ee6cc374ca1bc83fdb22c1529ffb350e8b
artbilan <artem.bilan13@gmail.com>
2022-01-24 13:05:49 +0200

    update

--- homework_23/test_url.py
+++ homework_23/test_url.py
@@ -8,13 +8,10 @@ def test_01():
     driver = Chrome("C:\\Users\\Art\\PycharmProjects\\main\\venv\\chromedriver\\chromedriver.exe")
     driver.get("https://github.com/artbilan?tab=repositories")
     driver.maximize_window()
-    sleep(3)
     search_button = driver.find_element(By.XPATH, '//a[@href="/artbilan/Hilel"]')
     search_button.click()
-    sleep(3)
     result_url = driver.current_url
     driver.minimize_window()
-    sleep(3)
     driver.close()
     assert result_url == exp_url
 
